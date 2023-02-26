284ca05db8a781ba829cc3b626cdd7ec1d27843b
Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
2023-02-25 16:51:24 -0800

    Center text, create navbar files

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,29 @@ import { TSE } from "./components/TSE";
 import { Tutor } from "./components/Tutor";
 
 import "./App.css";
+import { Navbar } from "./components/Navbar";
 
 export const App = () => {
   return (
     <div>
-      <h1>Brenton Dunn</h1>
-      <Education />
-      <h2>Relevant Coursework</h2>
+      <Navbar />
+      <div className="center">
+        <h1>Brenton Dunn</h1>
+        <Education />
+      </div>
+      <h2 className="center">Education</h2>
+      <h3>Relevant Coursework</h3>
       <ul>
         <li>Intro to Programming</li>
         <li>Discrete Mathemathics</li>
         <li>Data Structures & Object Oriented Design</li>
         <li>Mathematics for Algorithms</li>
       </ul>
-      <h2>Experience</h2>
+      <h2 className="center">Experience</h2>
       <TSE />
       <Tutor />
       <Boeing />
-      <h2>Skills</h2>
+      <h2 className="center">Skills</h2>
       <Skills />
     </div>
   );
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import "./Navbar.css";
+
+export const Navbar = () => {
+  return (
+    <div>
+      <p>Something</p>
+    </div>
+  );
+};
