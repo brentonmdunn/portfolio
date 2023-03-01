88301e64fd6ac1bd4bde50827f5145f38099f563
Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
2023-03-01 08:23:55 -0800

    Fix graduation date typo

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -6,7 +6,7 @@ export const Education = () => {
       <Experience
         title="Major: Computer Science  |  GPA: 3.93"
         organization="University of California San Diego"
-        dates="September 2022 - Present"
+        dates="September 2022 - June 2026"
         location="La Jolla, California"
       />
     </div>
